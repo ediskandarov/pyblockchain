cdbe33a2813d0ae89029ce80b5b04c9bfd05655d
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2016-05-27 23:21:21 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='pyblockchain',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Eduard Iskandarov',
+    author_email='eduard.iskandarov@yandex.ru',
+)
