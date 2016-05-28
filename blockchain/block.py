5ec2d2f872ccf230cbe3de15ff3294faac9e6ada
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2016-05-28 22:21:25 +0300

    add blockchain parser

--- blockchain/block.py
+++ blockchain/block.py
@@ -2,6 +2,29 @@ from hashlib import sha256
 import struct
 
 
+def varint(data, offset):
+    # variable length integer
+    # 1 byte unsigned int8
+    value, = struct.unpack('<B', data[offset:offset + 1])
+    bytes_consumed = 1
+
+    if value < 253:
+        pass
+    elif value == 253:
+        # 2 bytes unsigned int16
+        value, = struct.unpack('<H', data[offset + 1:offset + 3])
+        bytes_consumed += 2
+    elif value == 254:
+        # 4 bytes unsigned int32
+        value, = struct.unpack('<I', data[offset + 1:offset + 5])
+        bytes_consumed += 4
+    elif value == 255:
+        # 8 bytes unsigned int64
+        value, = struct.unpack('<Q', data[offset + 1:offset + 9])
+        bytes_consumed += 8
+    return value, bytes_consumed
+
+
 class BlockHeader(object):
     __slots__ = [
         'magic_number',
@@ -50,17 +73,127 @@ class BlockHeader(object):
         return '{:02x}'.format(self.magic_number)
 
 
+class Transaction(object):
+    __slots__ = ['version', 'inputs', 'outputs', 'lock_time']
+
+    def __init__(self, version, inputs, outputs, lock_time):
+        self.version = version
+        self.inputs = inputs
+        self.outputs = outputs
+        self.lock_time = lock_time
+
+    @classmethod
+    def from_binary_data(cls, data, offset):
+        version, = struct.unpack('<I', data[offset:offset + 4])
+        offset += 4
+
+        # Input transactions
+        txn_input_count, bytes_consumed = varint(data, offset=offset)
+        offset += bytes_consumed
+
+        txn_input_list = []
+        for i in range(txn_input_count):
+            txn_input, new_offset = TransactionInput.from_binary_data(
+                data,
+                offset=offset,
+            )
+            offset = new_offset
+            txn_input_list.append(txn_input)
+
+        # Output transactions
+        txn_output_count, bytes_consumed = varint(data, offset=offset)
+        offset += bytes_consumed
+
+        txn_output_list = []
+        for i in range(txn_output_count):
+            txn_output, new_offset = TransactionOutput.from_binary_data(
+                data,
+                offset=offset,
+            )
+            offset = new_offset
+            txn_output_list.append(txn_output)
+
+        lock_time, = struct.unpack('<I', data[offset:offset + 4])
+        offset += 4
+
+        return cls(version, txn_input_list, txn_output_list, lock_time), offset
+
+
+class TransactionInput(object):
+    __slots__ = ['prev_hash', 'txn_out_id', 'script_sig', 'seq_no']
+
+    def __init__(self, prev_hash, txn_out_id, script_sig, seq_no):
+        self.prev_hash = prev_hash
+        self.txn_out_id = txn_out_id
+        self.script_sig = script_sig
+        self.seq_no = seq_no
+
+    @classmethod
+    def from_binary_data(cls, data, offset):
+        prev_hash, txn_out_id = struct.unpack('<32sI', data[offset:offset + 36])
+        offset += 36
+
+        script_length, bytes_consumed = varint(data, offset=offset)
+        offset += bytes_consumed
+
+        script_sig, seq_no = struct.unpack(
+            '<{}sI'.format(script_length),
+            data[offset:offset + script_length + 4]
+        )
+        offset += script_length + 4
+
+        return cls(prev_hash, txn_out_id, script_sig, seq_no), offset
+
+
+class TransactionOutput(object):
+    __slots__ = ['value', 'public_key']
+
+    def __init__(self, value, public_key):
+        self.value = value
+        self.public_key = public_key
+
+    @classmethod
+    def from_binary_data(cls, data, offset):
+        value, = struct.unpack('<Q', data[offset:offset + 8])
+        offset += 8
+
+        public_key_length, bytes_consumed = varint(data, offset=offset)
+        offset += bytes_consumed
+
+        public_key, = struct.unpack(
+            '<{}s'.format(public_key_length),
+            data[offset:offset + public_key_length]
+        )
+        offset += public_key_length
+
+        return cls(value, public_key), offset
+
+
 class Block(object):
-    def __init__(self, block_data):
-        self.txn_count = 0
-        self.txns = []
+    __slots__ = ['header', 'transactions']
 
-        header_data = block_data[:88]
-        self.header = BlockHeader.from_binary_data(header_data)
+    def __init__(self, header, transactions):
+        self.header = header
+        self.transactions = transactions
 
-        # body data - already consumed header part
-        body_size = self.header.block_size - 80
-        body_data = block_data[88:body_size]
+    @classmethod
+    def from_binary_data(cls, block_data, offset):
+        txns = []
 
-    def consume_body_data(self, data):
-        pass
+        header_data = block_data[offset:offset + 88]
+        header = BlockHeader.from_binary_data(header_data)
+        offset += 88
+
+        txn_count, bytes_consumed = varint(block_data, offset=offset)
+        offset += bytes_consumed
+
+        for i in range(txn_count):
+            txn, new_offset = Transaction.from_binary_data(
+                block_data,
+                offset=offset
+            )
+            offset = new_offset
+
+            txns.append(txn)
+
+        return cls(header, txns)
--- tests/test_block.py
+++ tests/test_block.py
@@ -16,7 +16,7 @@ def test_genesis_block_headers():
             access=mmap.ACCESS_READ
         )
         blockchain_mview = memoryview(blockchain_mmap)
-        block = Block(blockchain_mview)
+        block = Block.from_binary_data(blockchain_mview, offset=0)
 
     assert block.header.magic_number_hex == 'd9b4bef9'
     assert block.header.block_size == 285
@@ -30,3 +30,28 @@ def test_genesis_block_headers():
     assert block.header.time == 1231006505
     assert '{:02x}'.format(block.header.bits) == '1d00ffff'
     assert block.header.nonce == 2083236893
+
+    assert len(block.transactions) == 1
+
+    txn = block.transactions[0]
+    assert txn.version == 1
+    assert len(txn.inputs) == 1
+    assert txn.lock_time == 0
+
+    txn_input = txn.inputs[0]
+    assert txn_input.prev_hash.hex() == (
+        '0000000000000000000000000000000000000000000000000000000000000000'
+    )
+    assert '{:02x}'.format(txn_input.seq_no) == 'ffffffff'
+    assert txn_input.txn_out_id == 4294967295
+    assert txn_input.script_sig.endswith(
+        b'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks'
+    )
+
+    assert len(txn.outputs) == 1
+    txn_output = txn.outputs[0]
+    assert txn_output.value == 50 * (10 ** 8)
+    assert txn_output.public_key.hex() == (
+        '4104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb'
+        '649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac'
+    )
