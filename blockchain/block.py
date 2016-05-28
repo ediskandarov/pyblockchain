647dd8c1fc213bd4f22e1fcebce4b7120ffa87f3
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2016-05-28 23:40:11 +0300

    add typing annotation

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,7 +1,8 @@
 import struct
+from typing import Sequence
 
 
-def varint(data, offset):
+def varint(data: memoryview, offset: int) -> (int, int):
     # variable length integer
     # 1 byte unsigned int8
     value, = struct.unpack('<B', data[offset:offset + 1])
@@ -38,14 +39,14 @@ class BlockHeader(object):
 
     def __init__(
             self,
-            magic_number,
-            block_size,
-            version,
-            previous_hash,
-            merkle_hash,
-            time,
-            bits,
-            nonce,
+            magic_number: int,
+            block_size: int,
+            version: int,
+            previous_hash: bytes,
+            merkle_hash: bytes,
+            time: int,
+            bits: int,
+            nonce: int,
     ):
         self.magic_number = magic_number
         self.block_size = block_size
@@ -57,32 +58,116 @@ class BlockHeader(object):
         self.nonce = nonce
 
     @classmethod
-    def from_binary_data(cls, data):
-        # unsigned int 4 bytes version
+    def from_binary_data(
+            cls,
+            data: memoryview,
+    ):
+        # unsigned int32 version
         # 32 bytes of previous hash
-        # 32 byres of merkle hash
-        # unsigned int 4 bytes time
-        # unsigned int 4 bytes of bits
-        # unsigned int 4 bytes of nonce
+        # 32 bytes of merkle hash
+        # unsigned int32 time
+        # unsigned int32 bits
+        # unsigned int32 nonce
         tup = struct.unpack('<III32s32sIII', data)
         return cls(*tup)
 
     @property
-    def magic_number_hex(self):
+    def magic_number_hex(self) -> str:
         return '{:02x}'.format(self.magic_number)
+
+
+class TransactionInput(object):
+    __slots__ = ['prev_hash', 'txn_out_id', 'script_sig', 'seq_no']
+
+    def __init__(
+            self,
+            prev_hash: bytes,
+            txn_out_id: int,
+            script_sig: bytes,
+            seq_no: int,
+    ):
+        self.prev_hash = prev_hash
+        self.txn_out_id = txn_out_id
+        self.script_sig = script_sig
+        self.seq_no = seq_no
+
+    @classmethod
+    def from_binary_data(
+            cls,
+            data: memoryview,
+            offset: int,
+    ):
+        prev_hash, txn_out_id = struct.unpack(
+            '<32sI',
+            data[offset:offset + 36]
+        )
+        offset += 36
+
+        script_length, bytes_consumed = varint(data, offset=offset)
+        offset += bytes_consumed
+
+        script_sig, seq_no = struct.unpack(
+            '<{}sI'.format(script_length),
+            data[offset:offset + script_length + 4]
+        )
+        offset += script_length + 4
+
+        return cls(prev_hash, txn_out_id, script_sig, seq_no), offset
+
+
+class TransactionOutput(object):
+    __slots__ = ['value', 'public_key']
+
+    def __init__(
+            self,
+            value: int,
+            public_key: bytes,
+    ):
+        self.value = value
+        self.public_key = public_key
+
+    @classmethod
+    def from_binary_data(
+            cls,
+            data: memoryview,
+            offset: int,
+    ):
+        value, = struct.unpack('<Q', data[offset:offset + 8])
+        offset += 8
+
+        public_key_length, bytes_consumed = varint(data, offset=offset)
+        offset += bytes_consumed
+
+        public_key, = struct.unpack(
+            '<{}s'.format(public_key_length),
+            data[offset:offset + public_key_length]
+        )
+        offset += public_key_length
+
+        return cls(value, public_key), offset
 
 
 class Transaction(object):
     __slots__ = ['version', 'inputs', 'outputs', 'lock_time']
 
-    def __init__(self, version, inputs, outputs, lock_time):
+    def __init__(
+            self,
+            version: int,
+            inputs: Sequence[TransactionInput],
+            outputs: Sequence[TransactionOutput],
+            lock_time: int,
+    ):
         self.version = version
         self.inputs = inputs
         self.outputs = outputs
         self.lock_time = lock_time
 
     @classmethod
-    def from_binary_data(cls, data, offset):
+    def from_binary_data(
+            cls,
+            data: memoryview,
+            offset: int,
+    ):
         version, = struct.unpack('<I', data[offset:offset + 4])
         offset += 4
 
@@ -118,70 +203,23 @@ class Transaction(object):
         return cls(version, txn_input_list, txn_output_list, lock_time), offset
 
 
-class TransactionInput(object):
-    __slots__ = ['prev_hash', 'txn_out_id', 'script_sig', 'seq_no']
-
-    def __init__(self, prev_hash, txn_out_id, script_sig, seq_no):
-        self.prev_hash = prev_hash
-        self.txn_out_id = txn_out_id
-        self.script_sig = script_sig
-        self.seq_no = seq_no
-
-    @classmethod
-    def from_binary_data(cls, data, offset):
-        prev_hash, txn_out_id = struct.unpack(
-            '<32sI',
-            data[offset:offset + 36]
-        )
-        offset += 36
-
-        script_length, bytes_consumed = varint(data, offset=offset)
-        offset += bytes_consumed
-
-        script_sig, seq_no = struct.unpack(
-            '<{}sI'.format(script_length),
-            data[offset:offset + script_length + 4]
-        )
-        offset += script_length + 4
-
-        return cls(prev_hash, txn_out_id, script_sig, seq_no), offset
-
-
-class TransactionOutput(object):
-    __slots__ = ['value', 'public_key']
-
-    def __init__(self, value, public_key):
-        self.value = value
-        self.public_key = public_key
-
-    @classmethod
-    def from_binary_data(cls, data, offset):
-        value, = struct.unpack('<Q', data[offset:offset + 8])
-        offset += 8
-
-        public_key_length, bytes_consumed = varint(data, offset=offset)
-        offset += bytes_consumed
-
-        public_key, = struct.unpack(
-            '<{}s'.format(public_key_length),
-            data[offset:offset + public_key_length]
-        )
-        offset += public_key_length
-
-        return cls(value, public_key), offset
-
-
 class Block(object):
     __slots__ = ['header', 'transactions']
 
-    def __init__(self, header, transactions):
+    def __init__(
+            self,
+            header: BlockHeader,
+            transactions: Sequence[Transaction],
+    ):
         self.header = header
         self.transactions = transactions
 
     @classmethod
-    def from_binary_data(cls, block_data, offset):
-        txns = []
-
+    def from_binary_data(
+            cls,
+            block_data: memoryview,
+            offset: int,
+    ):
         header_data = block_data[offset:offset + 88]
         header = BlockHeader.from_binary_data(header_data)
         offset += 88
@@ -189,6 +227,7 @@ class Block(object):
         txn_count, bytes_consumed = varint(block_data, offset=offset)
         offset += bytes_consumed
 
+        txns = []
         for i in range(txn_count):
             txn, new_offset = Transaction.from_binary_data(
                 block_data,
