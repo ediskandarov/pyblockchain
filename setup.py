9ba70f3cfc92fa0d614c5332fa68ef0e8f9a86f1
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2016-06-04 14:08:34 +0300

    prepare package for pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,22 @@ from setuptools import find_packages, setup
 setup(
     name='pyblockchain',
     version='0.1.0',
-    packages=find_packages(),
+    description=open('README.rst').read(),
+    url='https://github.com/toidi/pyblockchain',
     author='Eduard Iskandarov',
     author_email='eduard.iskandarov@yandex.ru',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='blockchain bitcoin altcoin',
+    packages=find_packages(),
+    install_requires=[
+        'base58',
+    ],
 )
