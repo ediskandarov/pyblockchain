2d58433748a94a114d59d1388e3a7668ffa39c99
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2016-06-05 23:04:43 +0300

    simplify test

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,25 +1,31 @@
 from datetime import datetime
-import os
-import mmap
+
+import pytest
 
 from blockchain.block import Block
 from blockchain.constants import Network
 
-blockchain_path = os.path.join(os.path.dirname(__file__), '1M.dat')
 
-
-def test_genesis_block_headers():
+@pytest.fixture
+def genesis_block():
     """https://en.bitcoin.it/wiki/Genesis_block"""
+    genesis_block_hex = (
+        'f9beb4d91d01000001000000000000000000000000000000000000000000000000000'
+        '00000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a'
+        '9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c0101000000010000000000000000000'
+        '000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104'
+        '455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206'
+        'f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b'
+        '73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd'
+        '6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7'
+        'ba0b8d578a4c702b6bf11d5fac00000000'
+    )
+    return bytes.fromhex(genesis_block_hex)
 
-    with open(blockchain_path, 'rb') as f:
-        # Map for 8MB block size
-        blockchain_mmap = mmap.mmap(
-            f.fileno(),
-            8 * 1024,
-            access=mmap.ACCESS_READ
-        )
-        blockchain_mview = memoryview(blockchain_mmap)
-        block = Block.from_binary_data(blockchain_mview, offset=0)
+
+def test_genesis_block_headers(genesis_block):
+    blockchain_mview = memoryview(genesis_block)
+    block = Block.from_binary_data(blockchain_mview, offset=0)
 
     assert block.header.magic_number == Network.mainnet.value
     assert block.header.block_size == 285
